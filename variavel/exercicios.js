2f6155a78f9f46c4e6d82497c1187bea3d286f64
jEduardo07 <112011173+jEduardo07@users.noreply.github.com>
2022-09-29 16:49:19 -0300

    Add files via upload

---
+++ variavel/exercicios.js
@@ -0,0 +1,15 @@
+var nome = 'Eduardo'
+console.log(nome);
+
+
+var a = 2
+var b = 5
+var resultado = 2*a*3*b
+console.log (resultado)
+
+var porcentagem = 0.15;
+var salario = 750
+
+var aumento = porcentagem * salario 
+var salarioatualizado = aumento + salario
+console.log(salarioatualizado)
---
+++ variavel/script.js
@@ -0,0 +1,108 @@
+console.log("Eduardo");
+
+var nome = 'Eduardo'
+console.log(nome);
+
+let idade2 = "26"
+console.log(idade2);
+
+const estudo = true;
+console.log(estudo)
+  
+
+var nome2 = "Eduardo"
+ idade3 = 20,
+ estuda = true;
+
+ //Variavel sem valor
+ var precoCarro;
+ console.log(precoCarro)
+
+
+ //regras dos nomes das variaveis
+ /*
+   Os nomes das variaveis podem iniciar com4, -, ou letras. Podem conter numeros mas inciar com eles.
+
+   Case sensitive
+   nome é diferente de NOME, Nome, noME ...
+
+   Não utilizar palavras reservadas.
+
+   Camel Case
+   É comum nomear assim: abrirCaixa
+ */
+
+   //Exemplos de nome de variaveis
+     
+   //Invalido
+   /*
+   var #nome
+   var function;
+   var 1pessoa
+
+   //Valido
+   var $selecionar
+   var _nome;
+   var pessoalpassou;
+   */
+
+   //Declarar
+
+   //console.log(Data)
+   //is not defined
+
+   //Mudar o valor atribuido
+   // E possivel mudar os valores atribuidos a variaveis declaradas com var e let porem não è possivel modificar volores das declaradas com cont.
+
+   var idade4 = 17;
+    idade4 = 29;
+   console.log(idade4)
+
+   let preco =50;
+   preco = 25;''
+   console.log(preco)
+
+   const possuiFaculdade = true;
+   //possuiFaculdade = false
+   console.log(possuiFaculdade);
+
+   var nome = 'Eduardo'
+console.log(nome);
+
+   var idade = '25'
+console.log(idade);
+
+   var comidaFavorita = 
+console.log(comidaFavorita);
+
+   var comidaFavorita = "pizza"
+console.log(comidaFavorita);
+ 
+var carro;
+
+var moto;
+
+var onibus;
+
+var aviao;
+
+var bike;
+
+
+//Revisão
+console.log("Erick")
+console.log(2*10)
+console.log(2 + 10)
+console.log(2 - 10)
+console.log(10 / 3)
+
+console.log(10 % 3)
+//resto da divisâo
+
+//Revisâo com calculos
+
+var valor1 = 7
+var valor2 = 2
+
+var resultado =valor1 + valor2
+console.log(resultado)
